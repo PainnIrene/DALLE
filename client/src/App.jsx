b0de634b2dfde0543140929274b51c26d8f544d9
PainnIrene <hoanghuytran10102003@gmail.com>
2023-05-15 07:47:17 +0700

    BUG

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import {BrowserRouter,Link,Route,Routes } from 'react-router-dom';
 
 import {logo} from './assets';
 import{Home,CreatePost} from './pages';
-const bodyParser = require("body-parser");
 const App = () => {
   return (
 <BrowserRouter>
